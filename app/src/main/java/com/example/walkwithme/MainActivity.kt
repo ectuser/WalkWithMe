fb77917c4e59f82b0d331b2e9eb93eda1412d558
Dima <dima.avanesov@yandex.ru>
2020-06-04 12:04:10 +0700

    Settings + launch
    Added osmdroid + osmdroidbonuspack. Tomsk is displayed on the screen on app launch.

--- app/src/main/java/com/example/walkwithme/MainActivity.kt
+++ app/src/main/java/com/example/walkwithme/MainActivity.kt
@@ -1,85 +1,62 @@
 package com.example.walkwithme
 
-import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.preference.PreferenceManager
-import android.util.Log
-import android.view.View
+import android.os.StrictMode
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import okhttp3.*
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.Request
+import okhttp3.Response
+import org.osmdroid.bonuspack.kml.KmlDocument
 import org.osmdroid.config.Configuration
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.CustomZoomButtonsController
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
 import java.io.IOException
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private val REQUEST_PERMISSIONS_REQUEST_CODE = 1
-    private var map: MapView? = null
-    private val client = OkHttpClient()
+    var REQUEST_PERMISSIONS_REQUEST_CODE = 1
+    var map: MapView? = null
+    var mKmlDocument: KmlDocument? = null
 
 
     public override fun onCreate(savedInstanceState: Bundle?) {
+        val policy =
+            StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+
         super.onCreate(savedInstanceState)
+        Configuration.getInstance().userAgentValue = "OBP_Tuto/1.0"
 
-        //handle permissions first, before map is created. not depicted here
-
-        //load/initialize the osmdroid configuration, this can be done
-        val ctx = applicationContext
-        Configuration.getInstance()
-            .load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx))
-        //setting this before the layout is inflated is a good idea
-        //it 'should' ensure that the map has a writable location for the map cache, even without permissions
-        //if no tiles are displayed, you can try overriding the cache path using Configuration.getInstance().setCachePath
-        //see also StorageUtils
-        //note, the load method also sets the HTTP User Agent to your application's package name, abusing osm's
-        //tile servers will get you banned based on this string
-
-        //inflate and create the map
         setContentView(R.layout.activity_main)
-        map = findViewById<View>(R.id.map) as MapView
-        map!!.setTileSource(TileSourceFactory.MAPNIK)
-        requestPermissionsIfNecessary(
-            arrayOf( // if you need to show the current location, uncomment the line below
-                // Manifest.permission.ACCESS_FINE_LOCATION,
-                // WRITE_EXTERNAL_STORAGE is required in order to show the map
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-            )
-        )
-
-        val pointFirst = GeoPoint(56.476313, 84.961030)
-        val pointSecond = GeoPoint(56.518821, 85.044477)
-
-        val startMarker = Marker(map)
-        startMarker.position = pointFirst
-        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-        map!!.overlays.add(startMarker)
-
-        val finishMarker = Marker(map)
-        finishMarker.position = pointSecond
-        finishMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-        map!!.overlays.add(finishMarker)
+        map = findViewById(R.id.map)
+        map!!.zoomController
+            .setVisibility(CustomZoomButtonsController.Visibility.SHOW_AND_FADEOUT)
+        map!!.setMultiTouchControls(true)
+        val startPoint = GeoPoint(56.4977, 84.9744)
+        val mapController = map!!.controller
+        mapController.setZoom(12.0)
+        mapController.setCenter(startPoint)
 
 
-        val apiKey = "98d38f94-d14c-425e-bf07-3704ca5d1491"
-        run("https://graphhopper.com/api/1/route?point=${pointFirst.latitude},${pointFirst.longitude}&point=${pointSecond.latitude},${pointSecond.longitude}&vehicle=foot&locale=en&calc_points=false&key=${apiKey}", startMarker)
+//        val apiKey = "98d38f94-d14c-425e-bf07-3704ca5d1491"
+//        run("https://graphhopper.com/api/1/route?point=${pointFirst.latitude},${pointFirst.longitude}&point=${pointSecond.latitude},${pointSecond.longitude}&vehicle=foot&locale=en&calc_points=false&key=${apiKey}", startMarker)
     }
 
-    private fun run(url: String, marker: Marker) {
-        val request = Request.Builder()
-            .url(url)
-            .build()
-
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {}
-            override fun onResponse(call: Call, response: Response) = handleResponse(response, marker)
-        })
-    }
+//    private fun run(url: String, marker: Marker) {
+//        val request = Request.Builder()
+//            .url(url)
+//            .build()
+//
+//        client.newCall(request).enqueue(object : Callback {
+//            override fun onFailure(call: Call, e: IOException) {}
+//            override fun onResponse(call: Call, response: Response) = handleResponse(response, marker)
+//        })
+//    }
 
     private fun handleResponse(response: Response, marker: Marker) {
         marker.title = response.body().toString()
