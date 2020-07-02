2246ad8279dacd6b0d97033a32f7dce0bc7f5a34
Dima <dima.avanesov@yandex.ru>
2020-07-02 20:36:41 +0700

    Avatar, dropdown for period
    Started styling Stats screen

--- app/src/main/java/com/example/walkwithme/fragments/StatsFragment.kt
+++ app/src/main/java/com/example/walkwithme/fragments/StatsFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import com.example.walkwithme.R
+import kotlinx.android.synthetic.main.fragment_stats.*
 
 class StatsFragment : Fragment() {
 
@@ -16,6 +18,9 @@ class StatsFragment : Fragment() {
     ): View? =
         inflater.inflate(R.layout.fragment_stats, container, false)
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {}
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val periodItems = arrayListOf("Today", "Last week", "Last month", "Last year", "All time")
+        PeriodSpinner.adapter = ArrayAdapter(requireContext(), R.layout.period_item, periodItems)
+    }
 
 }
